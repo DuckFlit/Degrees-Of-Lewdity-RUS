0c8ca421f3fc88a2490eefc772b5a17247cd23e3
Crimson Tide <literallyjustrandomletters@gmail.com>
2022-01-11 23:50:33 +0000

    Templates and bugfixes

--- game/03-JavaScript/03-Templates/bodyparts.js
+++
@@ -1,5 +0,0 @@
-Template.add('vulva',
-	either("vulva", "quim", "flange", "pussy"));
-
-Template.add('Vulva',
-	either("Vulva", "Quim", "Flange", "Pussy"));
---
+++ game/03-JavaScript/03-Templates/t-bodyparts.js
@@ -0,0 +1,15 @@
+/* ?vulva */
+Template.add('vulva',
+	either("vulva", "quim", "flange", "pussy"));
+
+/* ?Vulva */
+Template.add('Vulva',
+	either("Vulva", "Quim", "Flange", "Pussy"));
+
+/* ?bodypart */
+Template.add('bodypart',
+	either("hands", "feet", "thighs", "legs", "tummy", "cheek", "bottom", "chest", "face", "arms", "shoulder", "back"));
+
+/* ?lowerbodypart */
+Template.add('lowerbodypart',
+	either("hands", "feet", "thighs", "bottom"));
