7795559ae5c668d87386a95a603efac67a9e6e66
Vrelnir <vrelnir@gmail.com>
2022-03-27 18:03:25 +0100

    Updated to 0.3.8.6

--- game/01-config/sugarcubeConfig.js
+++ game/01-config/sugarcubeConfig.js
@@ -22,7 +22,7 @@ window.StartConfig = {
 	"debug": false,
 	"enableImages": true,
 	"enableLinkNumberify": true,
-	"version": "0.3.8.5",
+	"version": "0.3.8.6",
 }
 
 /* convert version string to numeric value */
