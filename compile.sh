3b8f1fc8a114bc5e4e4a423e6c19c2647e34497b
Jimmy <23598-Jimmys@users.noreply.gitgud.io>
2022-05-08 14:56:19 +0100

    Removing unnecessary symbol from compile.sh

--- compile.sh
+++ compile.sh
@@ -56,7 +56,7 @@ function compile() {
 		esac
 	fi
 
-	$TWEEGO_EXE "$@" -o  "$TARGET" --head "devTools/head.html" --module "%~dp0modules" game/ || build_failed="true"
+	$TWEEGO_EXE "$@" -o  "$TARGET" --head "devTools/head.html" --module "modules" game/ || build_failed="true"
 
 	if [ "$build_failed" = "true" ]; then
 		echoError "Build failed."
