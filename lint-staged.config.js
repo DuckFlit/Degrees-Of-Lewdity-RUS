e5ce7e09bfd2bb5a8993367babd8acb8633d8602
shikiyoku <14437-shikiyoku@users.noreply.gitgud.io>
2022-07-21 00:26:38 +0000

    Linting/Formatting guide update

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,6 @@
 export default {
-	"*.js": (filenames) => `eslint --cache --fix ${filenames.join(" ")}`,
+	"*.{js,cjs}": filenames => `eslint --cache --fix ${filenames.join(" ")}`,
 	"*.css": "stylelint --fix",
 	// Format other files with Prettier
-	"**/*.!js": "prettier --write --ignore-unknown",
+	"!(*.{js,cjs,css})": "prettier --ignore-unknown --write",
 };
