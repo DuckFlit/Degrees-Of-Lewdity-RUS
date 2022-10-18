9e28032c0a3b5adc51497566ae5af597d441a2ab
hwp <you@example.com>
2022-10-18 17:04:34 -0400

    Robin in Gwylan's Shop (for Halloween)

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 export default {
-	"*.{js,cjs}": filenames => `eslint --cache --fix ${filenames.join(" ")}`,
+	"*.{js,cjs}": filenames => `eslint --cache --fix "${filenames.join('" "')}"`,
 	"*.css": "stylelint --fix",
 	// Format other files with Prettier
 	"!(*.{js,cjs,css})": "prettier --ignore-unknown --write",
