86c913b030ef23293f07a77dd9864392a285306a
Jimmy <23598-Jimmys@users.noreply.gitgud.io>
2023-02-19 19:18:19 +0000

    base.css clean-up

--- stylelint.config.cjs
+++ stylelint.config.cjs
@@ -1,10 +1,6 @@
 module.exports = {
 	plugins: ["stylelint-no-unsupported-browser-features"],
-	extends: [
-		"stylelint-config-standard",
-		"stylelint-config-property-sort-order-smacss",
-		"stylelint-prettier/recommended",
-	],
+	extends: ["stylelint-config-standard", "stylelint-config-property-sort-order-smacss", "stylelint-prettier/recommended"],
 	rules: {
 		"plugin/no-unsupported-browser-features": [
 			true,
@@ -19,6 +15,15 @@ module.exports = {
 		"selector-class-pattern": null,
 		"selector-id-pattern": null,
 
+		/* Because we don't use PostCSS, these errors seem redundant. */
+		"media-feature-name-no-vendor-prefix": null,
+		"property-no-vendor-prefix": null,
+		"value-no-vendor-prefix": null,
+
+		/* Stylistic errors that aren't useful. */
+		"order/properties-order": null,
+		"comment-empty-line-before": null,
+
 		// Modified kebab-case for numbered CSS vars
 		"custom-property-pattern": [
 			"^([a-z0-9]*)(-[a-z0-9]+)*$",
@@ -27,4 +32,19 @@ module.exports = {
 			},
 		],
 	},
+	overrides: [
+		{
+			files: ["modules/css/base.css", "modules/css/clothing-shop-v2.css"],
+			rules: {
+				"font-family-no-missing-generic-family-keyword": null,
+				"no-descending-specificity": null,
+			},
+		},
+		{
+			files: ["modules/css/base.css"],
+			rules: {
+				"selector-type-no-unknown": null,
+			},
+		},
+	],
 };
