230adaa4239f8be79d5da82063a2fa1122ea0a05
xao <33469-xao321@users.noreply.gitgud.io>
2023-04-01 22:47:00 +0000

    Dev fixes

--- game/base-system/time/dateTime.js
+++ game/base-system/time/dateTime.js
@@ -7,8 +7,9 @@ class DateTime {
 	static monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 	static daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-	constructor(year = 2000, month = 1, day = 1, hour = 0, minute = 0, second = 0) {
+	constructor(year = 2020, month = 1, day = 1, hour = 0, minute = 0, second = 0) {
 		if (arguments.length === 1) {
+			// If the argument is a DateTime object, copy its properties
 			if (year instanceof DateTime) {
 				this.year = year.year;
 				this.month = year.month;
@@ -19,6 +20,7 @@ class DateTime {
 				this.timeStamp = year.timeStamp;
 				return;
 			}
+			// If the argument is a timestamp, validate it and set the DateTime object accordingly
 			if (arguments[0] < 0 || arguments[0] > 315569437199)
 				throw new Error("Invalid timestamp: Timestamp cannot be lower than 0 or higher than 315569437199.");
 			this.fromTimestamp(arguments[0]);
@@ -29,10 +31,10 @@ class DateTime {
 	}
 
 	/*
-	 * Total days since start of timeStamp calculation (year 0)
+	 * Total days since start of timeStamp calculation (year 1)
 	 */
 	static getTotalDaysSinceStart(year) {
-		return year * 365 + Math.floor(year / 4) - Math.floor(year / 100) + Math.floor(year / 400);
+		return ((year - 1) * 365) + Math.floor((year - 1) / 4) - Math.floor((year - 1) / 100) + Math.floor((year - 1) / 400);
 	}
 
 	static isLeapYear(year) {
@@ -47,6 +49,7 @@ class DateTime {
 		return DateTime.isLeapYear(year) ? 366 : 365;
 	}
 
+	// Convert year, month, day, hour, minute, and second to a timestamp
 	toTimestamp(year, month, day, hour, minute, second) {
 		if (year < 1 || year > 9999) throw new Error("Invalid year: Year must be between 1-9999.");
 		if (month < 1 || month > 12) throw new Error("Invalid month: Month must be between 1-12.");
@@ -65,17 +68,31 @@ class DateTime {
 		this.second = second;
 	}
 
+	// Convert a timestamp to year, month, day, hour, minute, and second
 	fromTimestamp(timestamp) {
-		let year = 0;
+		// Initialize the year to 1
+		let year = 1;
 		let month = 0;
 		let day = (timestamp / DateTime.secondsPerDay) | 0;
 		const hour = (timestamp / DateTime.secondsPerHour) | 0;
 		const minute = (timestamp / DateTime.secondsPerMinute) | 0;
 		const second = timestamp;
 
-		while (day >= DateTime.getDaysOfYear(year + 1)) day -= DateTime.getDaysOfYear(year++ + 1);
+		// Maps the total number of days to the corresponding year and day.
+		while (day > (DateTime.getDaysOfYear(year))){
+			day -= DateTime.getDaysOfYear(year++);
+		}
+
 		const daysPerMonth = DateTime.getDaysOfMonthFromYear(year);
-		while (day >= daysPerMonth[month]) day -= daysPerMonth[month++];
+
+		// Determines the month and day by subtracting the number of days in each month and incrementing the month value.
+		while (day >= daysPerMonth[month]){
+			day -= daysPerMonth[month++];
+			if (month > 11) {
+				month = 0;
+				year++;
+			}
+		}
 
 		this.timeStamp = timestamp;
 		this.year = year;
@@ -86,6 +103,7 @@ class DateTime {
 		this.second = second % 60;
 	}
 
+	// Returns the first occurrence of a given weekday (1-7 for Sun-Sat) in the current month.
 	getFirstWeekdayOfMonth(weekDay) {
 		if (weekDay < 1 || weekDay > 7) throw new Error("Invalid weekDay: Must be between 1-7");
 
@@ -93,6 +111,7 @@ class DateTime {
 		return date.addDays((weekDay - date.weekDay + 7) % 7);
 	}
 
+	// Returns the next occurrence of a given weekday (1-7 for Sun-Sat) after the current date.
 	getNextWeekdayDate(weekDay) {
 		if (weekDay < 1 || weekDay > 7) throw new Error("Invalid weekDay: Must be between 1-7");
 
@@ -101,6 +120,7 @@ class DateTime {
 		return date.addDays(days);
 	}
 
+	// Returns the previous occurrence of a given weekday (1-7 for Sun-Sat) before the current date.
 	getPreviousWeekdayDate(weekDay) {
 		if (weekDay < 1 || weekDay > 7) throw new Error("Invalid weekDay: Must be between 1-7");
 		const days = ((7 + weekDay - this.weekDay) % 7) - 7;
@@ -108,6 +128,8 @@ class DateTime {
 		return date.addDays(days);
 	}
 
+	// Adds the specified number of years to the current date
+	// Adding a negative value (e.g. -1) subtracts the years instead
 	addYears(years) {
 		if (years === 0) return this;
 		const newYear = this.year + years;
@@ -118,6 +140,8 @@ class DateTime {
 		return this;
 	}
 
+	// Adds the specified number of months to the current date
+	// Adding a negative value (e.g. -1) subtracts the months instead
 	addMonths(months) {
 		if (months === 0) return this;
 		const addedMonths = this.month + months;
@@ -129,12 +153,16 @@ class DateTime {
 		return this;
 	}
 
+	// Adds the specified number of days to the current date
+	// Adding a negative value (e.g. -1) subtracts the days instead
 	addDays(days) {
 		if (days === 0) return this;
 		this.fromTimestamp(this.timeStamp + days * DateTime.secondsPerDay);
 		return this;
 	}
 
+	// Adds the specified number of hours to the current date and time
+	// Adding a negative value (e.g. -1) subtracts the hours instead
 	addHours(hours) {
 		if (hours === 0) return this;
 		this.timeStamp += hours * DateTime.secondsPerHour;
@@ -142,6 +170,8 @@ class DateTime {
 		return this;
 	}
 
+	// Adds the specified number of minutes to the current date and time
+	// Adding a negative value (e.g. -1) subtracts the minutes instead
 	addMinutes(minutes) {
 		if (minutes === 0) return this;
 		this.timeStamp += minutes * DateTime.secondsPerMinute;
@@ -149,6 +179,8 @@ class DateTime {
 		return this;
 	}
 
+	// Adds the specified number of seconds to the current date and time
+	// Adding a negative value (e.g. -1) subtracts the seconds instead
 	addSeconds(seconds) {
 		if (seconds === 0) return this;
 		this.timeStamp += seconds;
@@ -156,35 +188,41 @@ class DateTime {
 		return this;
 	}
 
+	// Returns the weekday (1-7 for Sun-Sat) of the current object's date.
 	get weekDay() {
-		return (
-			1 +
-			((DateTime.getTotalDaysSinceStart(this.year) +
-				(DateTime.standardYearMonths.slice(0, this.month - 1).reduce((a, b) => a + b, 0) -
-					(DateTime.isLeapYear(this.year) && this.month < 3) +
-					this.day +
-					(V.weekDayOffset !== undefined ? V.weekDayOffset : 6))) % // teach the bird to f
-				7)
-		);
+		const daysSinceStart = DateTime.getTotalDaysSinceStart(this.year + 1);
+		const daysInMonth = DateTime.standardYearMonths.slice(0, this.month - 1).reduce((a, b) => a + b, 0);
+		const isLeapYear = DateTime.isLeapYear(this.year) && this.month < 3;
+		const weekDayOffset = V.weekDayOffset !== undefined ? V.weekDayOffset : 6;
+	  
+		const totalDays = daysSinceStart + daysInMonth + this.day + Number(isLeapYear) + weekDayOffset;
+		const weekDay = (totalDays % 7) + 1;
+	  
+		return weekDay;
 	}
 
+	// Returns the name of the weekday (e.g. "Sunday") of the current object's date.
 	get weekDayName() {
 		return DateTime.daysOfWeek[this.weekDay - 1];
 	}
 
+	// Returns the name of the month (e.g. "January") of the current object's date.
 	get monthName() {
 		return DateTime.monthNames[this.month - 1];
 	}
 
+	// Returns a boolean indicating whether the current object's date falls on a weekend (Saturday or Sunday).
 	get weekEnd() {
 		return this.weekDay === 7 || this.weekDay === 1;
 	}
 
+	// Returns the last date of the current month.
 	get lastDayOfMonth() {
 		const daysPerMonth = DateTime.getDaysOfMonthFromYear(this.year);
 		return daysPerMonth[this.month - 1];
 	}
 
+	// Returns the day of the year (1-365 or 1-366 for leap years) for the current object's date.
 	get yearDay() {
 		const daysPerMonth = DateTime.getDaysOfMonthFromYear(this.year);
 		return daysPerMonth.slice(0, this.month - 1).reduce((a, b) => a + b, 0) + this.day;
