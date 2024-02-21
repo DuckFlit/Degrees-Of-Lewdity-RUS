db404ee5c7fa8588c3cae979b669ece1803f649d
KnotLikeThis <48347-KnotLikeThis@users.noreply.gitgud.io>
2024-02-21 03:40:02 +0000

    cleaned up unnecessary code; naming changes

--- game/03-JavaScript/firepit.js
+++ game/03-JavaScript/firepit.js
@@ -11,7 +11,7 @@
  * @typedef {object} FirepitType
  * @property {number} lastsUntil timestamp in seconds at which the fire goes out
  * @property {number} maxItems maximum amount of items the firepit can hold at once
- * @property {number} maxFireTime the maximum lifetime of the fire at any point, in seconds
+ * @property {number} maxBurnTime the maximum lifetime of the fire at any point, in seconds
  * @property {number} cookMult how much a lit fire speeds up the cooking process - (0.5x, 2x, 3x etc)
  * @property {Object<string, number>} cookTime key value pairs of the items allowed in the firepit
  *                                              and their base required time to cook
@@ -29,7 +29,7 @@ const Firepit = (() => {
 			lastsUntil: V.timeStamp,
 			maxItems: 1,
 			cookMult: 2,
-			maxFireTime: 120 * 60, // 2 hours
+			maxBurnTime: 120 * 60, // 2 hours
 			cookTime: {},
 			items: [],
 		}, options);
@@ -39,28 +39,24 @@ const Firepit = (() => {
 	 * @param {FirepitType} fp
 	 * @returns {number} the time left before the fire goes out, in seconds
 	 */
-	function getFireTimeRaw(fp) {
+	function getBurnTime(fp) {
+		if (fp?.lastsUntil == null) return Errors.report("No firepit provided to getBurnTime");
 		return Math.max(fp.lastsUntil - V.timeStamp, 0);
 	}
 
 	/**
 	 * Adds the given time to the fire lifetime and updates the time required for all items in the firepit.
 	 *
-	 * Clamps the given time to the max lifetime allowed by `fp.maxFireTime`.
+	 * Clamps the given time to the max lifetime allowed by `fp.maxBurnTime`.
 	 *
 	 * @param {FirepitType} fp
 	 * @param {number} time the time to add to `fp.lastsUntil`, in seconds
-	 * @param {(FirepitType) => number} getFireTime (optional) callback that takes the firepit as an argument
-	 *                                   and gets the updated time left, in seconds.
-	 *                                   Used to apply extra modifiers when updating the
-	 *                                   time bonuses of items. Defaults to `Firepit.getFireTimeRaw`.
 	 */
-	function addFireTime(fp, time, getFireTime = getFireTimeRaw) {
+	function addBurnTime(fp, time) {
 		if (!fp || !Number.isInteger(time)) return;
 		const nowLastsUntil = Math.max(V.timeStamp, fp.lastsUntil) + time;
-		fp.lastsUntil = Math.min(nowLastsUntil, V.timeStamp + fp.maxFireTime);
-		const timeLeft = Number(getFireTime(fp)) || 0;
-		fp.items.forEach(item => updateTimeBonus(fp, item, timeLeft));
+		fp.lastsUntil = Math.min(nowLastsUntil, V.timeStamp + fp.maxBurnTime);
+		fp.items.forEach(item => updateTimeBonus(fp, item));
 	}
 
 	/**
@@ -72,22 +68,17 @@ const Firepit = (() => {
 	 * @param {FirepitType} fp
 	 * @param {FirepitItem} itemName the item to add to firepit, it's name must be a key in `fp.cookTime`
 	 * @param item
-	 * @param {object} options (optional)
-	 * @param {number} options.fireTimeLeft time left before the fire goes out, in seconds.
-	 *                                      Used only if updating the timeBonus of the given item.
-	 *                                      Use this if don't want the raw time left to be used.
-	 * @param {boolean} options.skipUpdateBonus if set to true, does not update the timeBonus of
-	 *                                          the item before adding it
+	 * @param {boolean} skipUpdateBonus if set to true, does not update the timeBonus of
+	 *                                  the item before adding it
 	 * @returns {void}
 	 */
-	function addItem(fp, item, options = {}) {
-		const { fireTimeLeft, skipUpdateBonus = false } = options;
+	function cookItem(fp, item, skipUpdateBonus = false) {
 		if (fp.items.length >= fp.maxItems || !item) return;
 		if (fp.cookTime[item.name] == null) {
 			return Errors.report(`Failed to add item ${item.name} to firepit: item not allowed`);
 		}
 		if (!skipUpdateBonus) {
-			updateTimeBonus(fp, item, fireTimeLeft);
+			updateTimeBonus(fp, item);
 		}
 		fp.items.push(item);
 	}
@@ -97,22 +88,18 @@ const Firepit = (() => {
 	 *
 	 * @param {FirepitType} fp
 	 * @param {string} itemName needs to be a key in `fp.cookTime`
-	 * @param {number} fireTimeLeft (optional) the fire time left in `fp` to use when
-	 *                              determining `item.timeBonus`, in seconds. Use this if you want
-	 *                              modifiers applied to the raw fire time left.
 	 * @returns {FirepitItem|undefined} the created item, if successful.
 	 */
-	function createItem(fp, itemName, fireTimeLeft = null) {
+	function createItem(fp, itemName) {
 		if (fp.cookTime[itemName] == null) {
-			return Errors.report(`Failed to add item ${itemName} to firepit: item not allowed`);
+			return Errors.report(`Failed to create item ${itemName}: item not allowed in provided firepit`);
 		}
 		const newItem = {
 			name: itemName,
 			startedAt: V.timeStamp,
 			timeBonus: 0,
 		};
-		if (fireTimeLeft == null) fireTimeLeft = getFireTimeRaw(fp);
-		updateTimeBonus(fp, newItem, fireTimeLeft);
+		updateTimeBonus(fp, newItem);
 		return newItem;
 	}
 
@@ -121,10 +108,9 @@ const Firepit = (() => {
 	 *
 	 * @param {FirepitType} fp
 	 * @param {FirepitItem} item item in `fp` getting it's bonus updated
-	 * @param {number} fireTimeLeft the fire time left in `fp` to use when updating `item.timeBonus`, in seconds
 	 */
-	function updateTimeBonus(fp, item, fireTimeLeft = null) {
-		item.timeBonus = getTimeBonus(fp, item, fireTimeLeft);
+	function updateTimeBonus(fp, item) {
+		item.timeBonus = getTimeBonus(fp, item);
 	}
 
 	/**
@@ -132,12 +118,11 @@ const Firepit = (() => {
 	 *
 	 * @param {FirepitType} fp
 	 * @param {FirepitItem} item item in `fp` getting it's bonus updated
-	 * @param {number} fireTimeLeft the fire time left in `fp` to use when updating `item.timeBonus`, in seconds
 	 * @returns {number} the total time, in seconds, that the item can skip from the
 	 *                   value in `fp.cookTime` (including bonus already applied)
 	 */
 	// prettier-ignore
-	function getTimeBonus(fp, item, fireTimeLeft = null) {
+	function getTimeBonus(fp, item) {
 		const cookMult = fp.cookMult;
 		const baseTime = fp.cookTime[item.name];
 		if (!baseTime || cookMult <= 0) return item.timeBonus;
@@ -149,9 +134,7 @@ const Firepit = (() => {
 		);
 		const elapsedTime = elapsedTimeRaw + elapsedTimeVirtual;
 		if (elapsedTime >= baseTime - item.timeBonus) return item.timeBonus;
-		if (fireTimeLeft == null) {
-			fireTimeLeft = getFireTimeRaw(fp);
-		}
+		const fireTimeLeft = getBurnTime(fp);
 		/*
 		 * bonus is the how much time will be ignored from baseTime
 		 * example: if cookMult is 3, ignores 2/3 of the time left
@@ -170,7 +153,7 @@ const Firepit = (() => {
 	 * @returns {FirepitItem[]}
 	 */
 	function getItemsReady(fp) {
-		return fp.items.filter(({ name, startedAt, timeBonus }) => {
+		return fp?.items.filter(({ name, startedAt, timeBonus }) => {
 			return V.timeStamp - startedAt >= fp.cookTime[name] - timeBonus;
 		});
 	}
@@ -182,7 +165,7 @@ const Firepit = (() => {
 	 * @returns {FirepitItem[]}
 	 */
 	function getItemsNotReady(fp) {
-		return fp.items.filter(({ name, startedAt, timeBonus }) => {
+		return fp?.items.filter(({ name, startedAt, timeBonus }) => {
 			return V.timeStamp - startedAt < fp.cookTime[name] - timeBonus;
 		});
 	}
@@ -190,11 +173,11 @@ const Firepit = (() => {
 	return Object.freeze({
 		create,
 
-		addFireTime,
-		getFireTimeRaw,
+		addBurnTime,
+		getBurnTime,
 
 		createItem,
-		addItem,
+		cookItem,
 		updateTimeBonus,
 		getTimeBonus,
 		getItemsReady,
@@ -203,13 +186,42 @@ const Firepit = (() => {
 })();
 window.Firepit = Firepit;
 
+function getBirdBurnTime() {
+	if (!V.bird?.firepit) return 0;
+	return Math.floor(Firepit.getBurnTime(V.bird.firepit) / 60); // minutes
+}
+window.getBirdBurnTime = getBirdBurnTime;
+
+/* each firepit would need it's own logic to apply upgrades */
+function upgradeBirdFirepit() {
+	const level = V.bird?.upgrades.firepit;
+	if (!Number.isInteger(level) || level < 1) return;
+	let maxBurnTime = 1480 * 60; // 24h40min
+	switch (level) {
+		case 1:
+			maxBurnTime = 360 * 60; // 6h
+			break;
+		case 2:
+			maxBurnTime = 740 * 60; // 12h20min
+			break;
+	}
+	Object.assign(V.bird.firepit, {
+		maxBurnTime,
+		maxItems: V.bird.upgrades.rack * 2,
+		cookMult: level + 1,
+	});
+	// 2/3 of max hours
+	const burnTime = Math.floor((maxBurnTime / 3600) * (2 / 3)) * 3600;
+	Firepit.addBurnTime(V.bird.firepit, burnTime);
+}
+window.upgradeBirdFirepit = upgradeBirdFirepit;
+
 /* ========================== PART BELOW GOES AWAY IF THIS IS IMPLEMENTED ========================== */
 
 /*
 	notes:
-	- Firepit.addFireTime:
+	- Firepit.addBurnTime:
 		- allows to put out the fire at any given time, updating the items seemlessly
-		- each firepit should have it's own function to apply modifiers to the time left if needed
 	- firepit.cookMult:
 		- if set to a value between 0 and 1, should allow to slow down the progress instead of speeding it up:
 				in theory, the firepit could be used as a fridge that stops items from going bad :D
@@ -220,56 +232,20 @@ window.Firepit = Firepit;
 			- to avoid this, either disallow upgrading when there are items in the firepit, or set item.startedAt to $timeStamp on upgrade.
 */
 
-window.getBirdFirepit = function (hour = 9, minute = 0) {
+function birdFirepitTest(hour = 9, minute = 0) {
 	Time.setTime(hour, minute);
 	// if this experiment works, $bird would get a firepit property that looks like this
 	const firepit = Firepit.create({
 		lastsUntil: V.timeStamp + 240 * 60, // 4 hours
 		maxItems: 2,
 		cookMult: 2,
-		maxFireTime: 360 * 60, // 6 hours
+		maxBurnTime: 360 * 60, // 6 hours
 		cookTime: {
 			lurker: 120 * 60, // 2 hours (test value)
 		},
 		items: [],
 	});
 
-	/* each firepit would need it's own logic to apply time modifiers */
-	const getBirdFireTime = (noModifiers = false) => {
-		// let burnTime = Firepit.getFireTimeRaw(V.bird.firepit);
-		let burnTime = Firepit.getFireTimeRaw(firepit);
-		if (V.weather !== "rain" || V.bird.upgrades.shelter > 0 || noModifiers) return burnTime;
-		burnTime = Math.floor(burnTime / 2);
-		if (burnTime === 0) firepit.lastsUntil = V.timeStamp; // fire went out
-		return burnTime;
-	};
-
-	/* each firepit would need it's own logic to apply upgrades */
-	const upgradeBirdFirepit = level => {
-		if (!level || !Number.isInteger(level) || firepit.items.length) return;
-		let maxFireTime = 1480 * 60; // 24h
-		switch (level) {
-			case 1:
-				maxFireTime = 360 * 60; // 6h
-				break;
-			case 2:
-				maxFireTime = 740 * 60; // 12h
-				break;
-		}
-		// Object.assign(V.bird.firepit, {
-		Object.assign(firepit, {
-			maxFireTime,
-			lastsUntil: V.timeStamp + maxFireTime,
-			maxItems: level * 2,
-			cookMult: level + 1,
-		});
-		const fireTimeLeft = getBirdFireTime();
-		firepit.items.forEach(item => Firepit.updateTimeBonus(firepit, item, fireTimeLeft));
-	};
-
-	return {
-		firepit,
-		getBirdFireTime,
-		upgradeBirdFirepit,
-	};
-};
+	return firepit;
+}
+window.birdFirepitTest = birdFirepitTest;
