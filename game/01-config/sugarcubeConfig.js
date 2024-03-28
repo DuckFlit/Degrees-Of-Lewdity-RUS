646eaf41f750aa62e154ca8dc4a8dedde8f3bfdd
Vrelnir <vrelnir@gmail.com>
2024-03-28 20:52:08 +0000

    Updated to 0.4.6.2

--- game/01-config/sugarcubeConfig.js
+++ game/01-config/sugarcubeConfig.js
@@ -11,9 +11,9 @@ window.StartConfig = {
 	debug: false,
 	enableImages: true,
 	enableLinkNumberify: true,
-	version: "0.4.6.1",
+	version: "0.4.6.2",
 	versionName: "",
-	sneaky: true,
+	sneaky: false,
 };
 
 State.prng.init();
