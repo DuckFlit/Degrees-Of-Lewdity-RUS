84274dd23719b8c07d807e0388304c283a2d730c
Purity <purityguydol@gmail.com>
2024-03-31 12:51:13 -0400

    Telescope and adjustments

--- game/03-JavaScript/04-Pregnancy/story-functions.js
+++ game/03-JavaScript/04-Pregnancy/story-functions.js
@@ -128,7 +128,7 @@ function npcPregnancyEnding(npc) {
 window.npcPregnancyEnding = npcPregnancyEnding;
 
 function birdEggsReady(npc) {
-	if (V.playerPregnancyEggLayingDisable === "t") return undefined;
+	if (V.playerPregnancyEggLayingDisable === "t" || !C.npc[npc] || C.npc[npc].vagina === "none") return undefined;
 	const pregnancy = C.npc[npc].pregnancy;
 	if (npcPregnancyEnding(npc)) return 'fertilised';
 	if (npc === 'Great Hawk' && V.daily.hawkUnfertilisedEggs) return undefined;
