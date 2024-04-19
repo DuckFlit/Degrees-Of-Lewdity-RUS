c2a0047e07fea4467d17c09548904c7542a90288
DuckFlit <55977510+DuckFlit@users.noreply.github.com>
2024-04-19 18:48:44 +0300

    Delete devTools directory

--- devTools/apkbuilder/build_app_debug.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# set up the environment
-ANDROID_HOME="$PWD/androidsdk/" \
-ANDROID_SDK_ROOT="$ANDROID_HOME" \
-ANDROID_PATH="$ANDROID_HOME/cmdline-tools/latest/bin" \
-PATH="./androidsdk/cmdline-tools/latest/bin:$PATH" \
-node scripts/build_app.js
