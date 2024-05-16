df5a72fd62d2fd0128e78f629dcc826d29942ce9
Purity <purityguydol@gmail.com>
2024-05-16 12:08:21 -0400

    Pregnancy adjustments

--- game/base-system/effect.js
+++ game/base-system/effect.js
@@ -255,7 +255,6 @@ function effects() {
 		V.squidcount = 0;
 	}
 
-
 	if (V.scienceproject === "ongoing" && V.scienceprojectdays === 0 && !V.scienceprojectwarning) {
 		V.scienceprojectwarning = 1;
 		element("span", `The science fair is being held in the town hall on Cliff Street today from ${ampm(9, 0)} until ${ampm(18, 0)}.`, "gold");
