58fb17877591e040c6f6ea53b2c72e19c682dc93
Vrelnir <vrelnir@gmail.com>
2024-07-17 03:37:21 +0100

    Updated to 0.5.1.1

--- game/01-config/sugarcubeConfig.js
+++ game/01-config/sugarcubeConfig.js
@@ -11,7 +11,7 @@ const StartConfig = {
 	debug: false,
 	enableImages: true,
 	enableLinkNumberify: true,
-	version: "0.5.1.0",
+	version: "0.5.1.1",
 	versionName: `"Head in the Clouds" edition`,
 	sneaky: true,
 	socialMediaEnabled: true,
